f8f587bdf9486d1d8867ff9e041295a892208770
xahid07 <56739408+xahid07@users.noreply.github.com>
2023-07-26 17:10:04 +0600

    Update vanillascript.js

--- pt-slider/vanillascript.js
+++ pt-slider/vanillascript.js
@@ -1,147 +1,134 @@
+const ___ = (parent,element) => parent.querySelector(element);
+const ____ = (parent,element) => parent.querySelectorAll(element);
 class PTSlider {
+     'use strict';
      constructor(e) {
-          this.e = e;
-          PTSlider.init(this.e)
-     }
-     static init(e) {
-          const wraper = document.querySelector(e);
-          wraper.classList.add('pt_slider_wraper');
-          wraper.firstElementChild.classList.add('pt_slider');
-          // const wraperId=wraper.getAttribute('id');
-          const slider = wraper.querySelector('.pt_slider');
-          const items = slider.querySelectorAll('.slider_item');
-          const itemCount = items.length;
-          const lastMargin = parseInt(getComputedStyle(items[itemCount - 1]).getPropertyValue('margin-right'));
-          const sliderWidth = slider.scrollWidth + lastMargin;
-          const viewWidth = slider.offsetWidth;
-
+          const a = this;
+          Object.assign(a,{
+                    e:e,
+                    wraper: ___(document,e),
+                    slider:'',
+                    items:[],
+                    itemCount:0,
+                    lastMargin:0,
+                    sliderWidth:0,
+                    viewWidth:0,
+                    navigationBullet:'',
+                    navigatorArea:0,
+                    last_transform_value:0,
+                    minNavWidth:0,
+                    slidingLength:0,
+                    SlidingAbs:0,
+                    navMovingArea:0,
+                    startX:0,
+                    offsetX:0,
+                    newX:0,
+                    lastAbs:0,
+                    newAbs:0,
+                    navLength:0,
+                    lastX:0,
+                    newPlacement:0,
+                    navLengthAbs:0,
+                    minDistance:10,
+               }
+          )
+          a.wraper.classList.add('pt_slider_wraper');
+          a.wraper.firstElementChild.classList.add('pt_slider');
+          a.slider = ___(a.wraper,'.pt_slider');
+          a.items = ____(a.slider,'.slider_item');
+          a.itemCount = a.items.length;
+          a.lastMargin = parseInt(getComputedStyle(a.items[a.itemCount - 1]).getPropertyValue('margin-right'));
+          a.sliderWidth = a.slider.scrollWidth + a.lastMargin;
+          a.viewWidth = a.slider.offsetWidth;
+          // =====================================================
           // Slider action close due to not being lengthy enough...
-          if (sliderWidth - lastMargin <= viewWidth) return false;
-          const last_transform_value = sliderWidth - viewWidth;
-          console.log(!wraper.querySelector('.pt_slider_navigator'))
-          if(!wraper.querySelector('.pt_slider_navigator')){
+          if (a.sliderWidth - a.lastMargin <= a.viewWidth) return false;
+          // =====================================================
+          a.init();
+     }
+     #creatingProgressBar = ()=>{
+          const a=this;
+          a.last_transform_value = a.sliderWidth - a.viewWidth;
+          if(!___(a.wraper,'.pt_slider_navigator')){
                const tempContainer = document.createElement('div');
                const htmlSinppet = '<div class="pt_slider_navigator"><div class="navigator_bullet"></div></div>';
                tempContainer.innerHTML = htmlSinppet;
                while (tempContainer.firstChild) {
-                    wraper.appendChild(tempContainer.firstChild);
+                    a.wraper.appendChild(tempContainer.firstChild);
                }
           }
-          console.log('last_transform_value :>> ', last_transform_value);
-          const navigationBullet = wraper.querySelector('.navigator_bullet');
-          const navigatorArea = wraper.querySelector('.pt_slider_navigator').clientWidth;
-          let itemLength = 0;
-          const itemObj = [];
-          items.forEach((item, i) => {
-               item.setAttribute('data-index', i);
-               const itemWidth = item.offsetWidth;
-               // const transformLength=Math.round(itemWidth);
-               const styles = getComputedStyle(item);
-               const m_l = parseInt(styles.getPropertyValue('margin-left'));
-               const m_r = parseInt(styles.getPropertyValue('margin-right'));
-               const m_t = parseInt(styles.getPropertyValue('margin-top'));
-               const m_b = parseInt(styles.getPropertyValue('margin-bottom'));
-               const tempObj = {
-                    'index': i,
-                    'm-l': m_l,
-                    'm-r': m_r,
-                    'm-t': m_t,
-                    'm-b': m_b,
-                    'itemWidth': itemWidth,
-                    'innerText': item.textContent.trim(),
-                    'transform': last_transform_value < itemLength ? -last_transform_value : -itemLength
-               }
-               itemLength += itemWidth + m_l + m_r;
-               itemObj.push(tempObj);
-          })
-          console.log('itemObj :>> ', itemObj);
-          const minNavWidth = Math.round((viewWidth / (sliderWidth)) * 100);
-          navigationBullet.style.setProperty('width',`${minNavWidth}px`);
-          const slidingLength = Math.round(itemObj[itemCount - 1].transform);
-          const SlidingAbs = Math.abs(slidingLength);
-          const navMovingArea = navigatorArea - minNavWidth;
-
-
-          let startX, offsetX, newX, lastAbs, newAbs, navLength, lastX = 0, newPlacement = 0, navLengthAbs = 0, minDistance = 10;
-          slider.addEventListener('mousedown', function (e) {
-               startX = e.clientX;
-               // offsetX = parseInt(this.css('left'), 10);
-               offsetX = parseInt(getComputedStyle(this).getPropertyValue('left'));
-               document.addEventListener('mousemove', dragSlider);
-          });
-
-          // Add mouseup event listener
-          document.addEventListener('mouseup', function () {
-               // Remove the mousemove event listener
-               lastX = newX;
-               newPlacement = parseInt(getComputedStyle(slider).getPropertyValue('--x-exis'));
-               if (newPlacement > 0) {
-                    slider.style.setProperty('--x-exis', '0px');
-                    newPlacement = lastX = 0;
-               } else if (newPlacement < slidingLength) {
-                    slider.style.setProperty('--x-exis', slidingLength + 'px');
-                    newPlacement = lastX = slidingLength;
-               }
-
-               document.removeEventListener('mousemove', dragSlider);
-          });
-
-          // Touch evnets...
-          slider.addEventListener('touchstart', function (e) {
-               startX = e.touches[0].clientX;
-               offsetX = parseInt(getComputedStyle(this).getPropertyValue('left'));
-               document.addEventListener('touchmove', swiping);
-           });
-           
-           // Add touchend event listener
-           document.addEventListener('touchend', function () {
-               // Remove the touchmove event listener
-               lastX = newX;
-               newPlacement = parseInt(getComputedStyle(slider).getPropertyValue('--x-exis'));
-               if (newPlacement > 0) {
-                   slider.style.setProperty('--x-exis', '0px');
-                   newPlacement = lastX = 0;
-               } else if (newPlacement < slidingLength) {
-                   slider.style.setProperty('--x-exis', slidingLength + 'px');
-                   newPlacement = lastX = slidingLength;
-               }
-           
-               document.removeEventListener('touchmove', swiping);
-           });
-
-           // Function to handle Swiping events
-          function swiping(e) {
-               newX = offsetX + e.touches[0].clientX - startX;
-               lastAbs = Math.abs(lastX); newAbs = Math.abs(newX);
-               const checkValue = lastAbs > newAbs ? lastAbs - newAbs : newAbs - lastAbs;
-               if (checkValue > minDistance) {
-               navLength = Math.round(newPlacement + newX);
-               slider.style.setProperty('--x-exis', navLength + 'px');
-               navLengthAbs = Math.abs(navLength);
-               if (navLength <= 0 && navLengthAbs <= SlidingAbs) {
-                    const navWidth = Math.round(navLengthAbs * navMovingArea / SlidingAbs + minNavWidth);
-                    navigationBullet.style.setProperty('width', navWidth + 'px');
-               }
-               // console.log('newX :>> ', minNavWidth);
+          a.navigationBullet = ___(a.wraper,'.navigator_bullet');
+          a.navigatorArea = ___(a.wraper,'.pt_slider_navigator').clientWidth;
+     }
+     #calculation = ()=>{
+          const a = this;
+          a.minNavWidth = Math.round((a.viewWidth / (a.sliderWidth)) * 100);
+          a.navigationBullet.style.setProperty('width',`${a.minNavWidth}px`);
+          a.slidingLength = Math.round(-a.last_transform_value);
+          a.SlidingAbs = Math.abs(a.slidingLength);
+          a.navMovingArea = a.navigatorArea - a.minNavWidth;
+     }
+     // Function to handle dragging
+     #dragSlider = (e) => {
+          const a = this;
+          a.newX = (a.offsetX + e.clientX - a.startX) || (a.offsetX + e.touches && e.touches[0].clientX - a.startX);
+          a.lastAbs = Math.abs(a.lastX); 
+          a.newAbs = Math.abs(a.newX);
+          const checkValue = a.lastAbs > a.newAbs ? a.lastAbs - a.newAbs : a.newAbs - a.lastAbs;
+          if (checkValue > a.minDistance) {
+               a.navLength = Math.round(a.newPlacement + a.newX);
+               a.slider.style.setProperty('--x-exis', a.navLength + 'px');
+               a.navLengthAbs = Math.abs(a.navLength);
+               if (a.navLength <= 0 && a.navLengthAbs <= a.SlidingAbs) {
+                    const navWidth = Math.round(a.navLengthAbs * a.navMovingArea / a.SlidingAbs + a.minNavWidth);
+                    a.navigationBullet.style.setProperty('width', navWidth + 'px');
                }
           }
-
-          // Function to handle dragging
-          function dragSlider(e) {
-               // if(navMovingArea<0){navigationBullet.hide();return false}
-               newX = offsetX + e.clientX - startX;
-               lastAbs = Math.abs(lastX); newAbs = Math.abs(newX);
-               const checkValue = lastAbs > newAbs ? lastAbs - newAbs : newAbs - lastAbs;
-               if (checkValue > minDistance) {
-                    navLength = Math.round(newPlacement + newX);
-                    slider.style.setProperty('--x-exis', navLength + 'px');
-                    navLengthAbs = Math.abs(navLength);
-                    if (navLength <= 0 && navLengthAbs <= SlidingAbs) {
-                         const navWidth = Math.round(navLengthAbs * navMovingArea / SlidingAbs + minNavWidth);
-                         navigationBullet.style.setProperty('width', navWidth + 'px');
-                    }
-                    // console.log('newX :>> ', minNavWidth);
+     }
+     #eventStartFunc = (e)=>{
+          const a = this;
+          a.startX = e.clientX || e.touches && e.touches[0].clientX;
+               a.offsetX = parseInt(getComputedStyle(this.slider).getPropertyValue('left'));
+               document.addEventListener('mousemove', a.#dragSlider);
+               document.addEventListener('touchmove', a.#dragSlider);
+     }
+     #eventEndFunct = (e) => {
+          const a = this;
+          a.lastX = a.newX;
+               a.newPlacement = parseInt(getComputedStyle(a.slider).getPropertyValue('--x-exis'));
+               if (a.newPlacement > 0) {
+                    a.slider.style.setProperty('--x-exis', '0px');
+                    a.newPlacement = a.lastX = 0;
+               } else if (a.newPlacement < a.slidingLength) {
+                    a.slider.style.setProperty('--x-exis', a.slidingLength + 'px');
+                    a.newPlacement = a.lastX = a.slidingLength;
                }
-          }
+               document.removeEventListener('mousemove', a.#dragSlider);
+               document.removeEventListener('touchmove', a.#dragSlider);
+     }
+     #mouseDownEvent = ()=>{
+          const a = this;
+          a.slider.addEventListener('mousedown', a.#eventStartFunc);
+     }
+     #touchStartEvent = () => {
+          const a = this;
+          a.slider.addEventListener('touchstart', a.#eventStartFunc);
+     }
+     #mouseUpEvent = ()=>{
+          const a = this;
+          document.addEventListener('mouseup', a.#eventEndFunct);
+     }
+     #mouseEndEvent = () => {
+          const a = this;
+          document.addEventListener('touchend', a.#eventEndFunct);
+     }
+     init() {
+          const a = this;
+          a.#creatingProgressBar();
+          a.#calculation();
+          a.#mouseDownEvent();
+          a.#touchStartEvent();
+          a.#mouseUpEvent();
+          a.#mouseEndEvent();
      }
 }
